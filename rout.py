1c5d06682ab43ae5ea71f03c3fd8c375b6a7183a
Kravchenko-Daniil <90443315+Kravchenko-Daniil@users.noreply.github.com>
2024-03-15 14:07:02 +0300

    Add files via upload

--- rout.py
+++ rout.py
@@ -9,6 +9,7 @@ class RoutToSite:
     def __init__(self):
         opts = webdriver.ChromeOptions()
         opts.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36")
+        opts.add_experimental_option("detach", True)
         self.driver = webdriver.Chrome(options=opts)
         self.url = 'https://www.okx.com/ru/account/users'
 
@@ -25,9 +26,5 @@ class RoutToSite:
             self.driver.find_element(By.XPATH, '//*[@id="login_phone_password_id"]').send_keys(password)
             time.sleep(1)
             self.driver.find_element(By.XPATH, '//*[@id="login-submit-btn"]').click()
-            time.sleep(60)
         except Exception as e:
             print(e)
-        finally:
-            self.driver.close()
-            self.driver.quit()
